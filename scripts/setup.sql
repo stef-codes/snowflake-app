809dfbb06ce0e03ff911e7b3862b28ca95b3206c
Stefanie <stefcodes@gmail.com>
2023-08-07 14:58:44 -0700

    add logic for setup file

--- scripts/setup.sql
+++ scripts/setup.sql
@@ -1,1 +1,16 @@
 -- Setup script for the Hello Snowflake! application.
+CREATE APPLICATION ROLE app_public; 
+CREATE SCHEMA IF NOT EXISTS core; 
+GRANT USAGE PN SCHEMA core TO APPLICATION ROLE app_public; 
+
+CREATE OR REPLACE PROCEDURE CORE.HELLO()
+    RETURNS STRING
+    LANGUAGE SQL
+    EXECUTE AS OWNER 
+    AS 
+    BEGIN
+        RETURN 'Heyyyyyy Snowflake'; 
+    END; 
+
+GRANT USAGE ON PROCEDURE core.hello() TO APPLICATION ROLE app_public; 
+
